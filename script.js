8f41b7b9b09dfe07ec9abe95b819c374768e84b8
teronayne <60113034+teronayne@users.noreply.github.com>
2021-06-09 20:30:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const balloon = document.querySelector('balloon')
+const cloudOne = document.querySelector('cloud-one')
+const cloudTwo = document.querySelector('cloud-two')
+const cloudThree = document.querySelector('cloud-three')
+const cloudFour = document.querySelector('cloud-four')
+const cloudFive = document.querySelector('cloud-five')
+const javascript = document.querySelector('javascript')
+const react = document.querySelector('react')
+const graphql = document.querySelector('graphql')
+const balloonYFromBottom = 10
+
+balloon.style.bottom = balloonYFromBottom + '%'
+
+function move() {
+  const incrementer = window.scrollY
+
+  balloon.style.bottom = balloonYFromBottom + incrementer * 0.1 + '%'
+}
+
+window.addEventListener('scroll', move)
